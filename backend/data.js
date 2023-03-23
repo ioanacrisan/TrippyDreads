ccca7eb7ccbccaab5321e5bfc0ba3390e92ed033
IOANA-NARCISA CRIȘAN <crisanioana@protonmail.com>
2023-03-23 11:55:13 +0200

    HomeScreen, ProductScreen, AddToCart

---
+++ backend/data.js
@@ -0,0 +1,32 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Deep Blue Sea - Full Set',
+      slug: 'deep-blue-sea-set',
+      category: 'Full Sets',
+      image: 'images/deep-blue-sea.jpg',
+      price: 250,
+      countInStock: 4,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Take a dive into the mesmerizing sea, at night you might notice its special iluminating magic!',
+    },
+
+    {
+      _id: '2',
+      name: 'Cloudy Sunset - Full Set',
+      slug: 'cloudy-sunset-set',
+      category: 'Full Sets',
+      image: 'images/cloudy-sunset.jpg',
+      price: 250,
+      countInStock: 1,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Do you remember an evening with a magical sunset? Even the fact that there were clouds made it special',
+    },
+  ],
+};
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,53 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import Badge from 'react-bootstrap/esm/Badge';
+import Nav from 'react-bootstrap/esm/Nav';
+import { useContext } from 'react';
+import { Store } from './Store';
+
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">Trippy Dreads</a>
-      </header>
-      <main>Product list</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Trippy Dreads</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
