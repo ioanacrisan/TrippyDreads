7e59500fb1161505662324409dded16ec47a2492
IOANA-NARCISA CRIȘAN <crisanioana@protonmail.com>
2023-03-27 22:44:37 +0300

    Create Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,27 @@ productRouter.get('/', async (req, res) => {
   res.send(products);
 });
 
+productRouter.post(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const newProduct = new Product({
+      name: 'sample name ' + Date.now(),
+      slug: 'sample-name-' + Date.now(),
+      category: 'sample category',
+      image: '/images/p1.jpg',
+      price: 0,
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: 'sample description',
+    });
+    const product = await newProduct.save();
+    res.send({ message: 'Product Created', product });
+  })
+);
+
 const PAGE_SIZE = 3;
 
 productRouter.get(
